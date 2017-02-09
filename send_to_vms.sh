4a07fb4cdbfd243d897612a46a6ed3fb163f5dee
lizhuo <lizhuogo@gmail.com>
2017-02-09 10:15:58 -0600

    MOD: finish sending script

--- send_to_vms.sh
+++ send_to_vms.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
 echo -n Username:
 read username
 
 echo -n Password:
 read -s password
 
-scp -r res/ dist/ $username@sp17-cs425-g03-01.cs.illinois.edu:~
+for i in {1..9}
+do
+    sshpass -p $password scp -r res/ dist/ $username@sp17-cs425-g03-0$i.cs.illinois.edu:~
+    echo "[SUCCESS] Send to sp17-cs425-g03-0$i.cs.illinois.edu"
+done
+
+sshpass -p $password scp -r res/ dist/ $username@sp17-cs425-g03-10.cs.illinois.edu:~
+echo "[SUCCESS] Send to sp17-cs425-g03-10.cs.illinois.edu"
