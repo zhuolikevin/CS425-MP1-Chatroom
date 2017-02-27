0c025546918d6df5c4a2c751116968e9d5414a9c
lizhuo <lizhuogo@gmail.com>
2017-02-26 19:00:27 -0600

    ADD: Failure detection

--- src/ConnectionListener.java
+++ src/ConnectionListener.java
@@ -1,14 +1,20 @@
 import java.io.IOException;
 import java.net.Socket;
 import java.net.ServerSocket;
-import mputil.NodeNotifHandler;
+
+import mputil.*;
 
 public class ConnectionListener implements Runnable {
   private Socket connectedClient = null;
   private ServerSocket server;
+  private Node thisNode;
+
   private NodeNotifHandler notifHandler = new NodeNotifHandler();
 
-  public ConnectionListener(ServerSocket server) { this.server = server; }
+  public ConnectionListener(ServerSocket server, Node thisNode) {
+    this.server = server;
+    this.thisNode = thisNode;
+  }
 
   /**
    * Listening for connections from other nodes
@@ -21,7 +27,12 @@ public class ConnectionListener implements Runnable {
         e.printStackTrace();
       }
       notifHandler.printNoticeMsg("Connected with " + connectedClient.getRemoteSocketAddress());
-      new Thread(new MessageReceiver(connectedClient)).start();
+
+      IpTools tool = new IpTools();
+      String ip = tool.parseIpPort(connectedClient.getRemoteSocketAddress().toString().substring(1))[0];
+      thisNode.putSocketToServerSockMap(ip, connectedClient);
+
+      new Thread(new MessageReceiver(connectedClient, thisNode)).start();
     }
   }
 }
